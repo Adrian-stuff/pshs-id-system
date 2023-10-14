ee9625ffc2381e6450351190817ae80648a9ea97
Adrian De Vera <twoadrian6@gmail.com>
2023-10-14 13:53:43 +0800

    add footer and change title

--- app/page.tsx
+++ app/page.tsx
@@ -217,268 +217,271 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col gap-2 mt-2 ml-2">
-      <div className="flex flex-row gap-2">
-        <div className="flex flex-col gap-2">
-          {IDCanvas(
-            stageRef,
-            stageSize,
-            lastNameStyle,
-            nameStyle,
-            setOr,
-            studentNameIndex,
-            secStyle,
-            secText,
-            rdStyle,
-            photoStyle,
-            setPhotoStyle,
-            guardianNameStyle,
-            guardianNameIndex,
-            contactNumStyle,
-            contactNumberIndex,
-            addressStyle,
-            addressIndex,
-            adviserStyle,
-            adviserText,
-            lrnIndex,
-            lrnStyle,
-            photoImage,
-            isStem
-          )}
-          <div className="grid grid-cols-5 max-w-xl gap-2">
-            {profileImages.map((val, idx) => {
-              const imageUrl = URL.createObjectURL(val);
-              return (
-                <button
-                  className="flex flex-col items-center gap-1 max-w-[125px] "
-                  onClick={() => {
-                    console.log(idx);
-                    setPhotoImage(imageUrl);
-                  }}
-                  key={idx}
-                >
-                  <img src={imageUrl} alt="" />
-                  <p className="text-center break-all">{val.name}</p>
-                </button>
-              );
-            })}
-          </div>
-        </div>
-        <div className="flex flex-row">
-          {/* <DataImport></DataImport> */}
-          <div>
-            {dataSheet.length !== 0 && (
-              <div>
-                <div>
-                  {SelectField(
-                    "First Name",
-                    setStudentFirstNameIndex,
-                    dataSheet
-                  )}
-                  {SelectField(
-                    "Middle Name",
-                    setStudentMiddleNameIndex,
-                    dataSheet
-                  )}
-                  {SelectField("Last Name", setStudentLastNameIndex, dataSheet)}
-                  {SelectField("Suffix", setStudentSuffixIndex, dataSheet)}
-                </div>
-
-                {SelectField("Guardian Name", setGuardianNameIndex, dataSheet)}
-                {SelectField(
-                  "Contact Number",
-                  setContactNumberIndex,
-                  dataSheet
-                )}
-
-                {SelectField("Address", setAddressIndex, dataSheet)}
-                {SelectField("LRN", setLrnIndex, dataSheet)}
-              </div>
+    <div>
+      <div className="flex flex-col gap-2 mt-2 ml-2">
+        <div className="flex flex-row gap-2">
+          <div className="flex flex-col gap-2">
+            {IDCanvas(
+              stageRef,
+              stageSize,
+              lastNameStyle,
+              nameStyle,
+              setOr,
+              studentNameIndex,
+              secStyle,
+              secText,
+              rdStyle,
+              photoStyle,
+              setPhotoStyle,
+              guardianNameStyle,
+              guardianNameIndex,
+              contactNumStyle,
+              contactNumberIndex,
+              addressStyle,
+              addressIndex,
+              adviserStyle,
+              adviserText,
+              lrnIndex,
+              lrnStyle,
+              photoImage,
+              isStem
             )}
+            <div className="grid grid-cols-5 max-w-xl gap-2">
+              {profileImages.map((val, idx) => {
+                const imageUrl = URL.createObjectURL(val);
+                return (
+                  <button
+                    className="flex flex-col items-center gap-1 max-w-[125px] "
+                    onClick={() => {
+                      console.log(idx);
+                      setPhotoImage(imageUrl);
+                    }}
+                    key={idx}
+                  >
+                    <img src={imageUrl} alt="" />
+                    <p className="text-center break-all">{val.name}</p>
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex flex-row">
+            {/* <DataImport></DataImport> */}
             <div>
-              <Label>Student Pictures:</Label>
-              <Input
-                type="file"
-                name="images"
-                onChange={onImagesChange}
-                multiple
-                accept="image/*"
-              ></Input>
-              <div>
-                <Dialog open={isCropOpen}>
-                  <DialogTrigger asChild>
-                    <div className="flex w-full items-center justify-center">
-                      <Button
-                        className="my-2 "
-                        onClick={() => setIsCropOpen(true)}
-                      >
-                        Edit Image
-                      </Button>
-                    </div>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <Cropper ref={cropperRef} src={photoImage}></Cropper>
-                    <Button onClick={() => onCrop()}>Crop</Button>
-                    <DialogClose asChild>
-                      <Button onClick={() => setIsCropOpen(false)}>
-                        Cancel
-                      </Button>
-                    </DialogClose>
-                  </DialogContent>
-                </Dialog>
-              </div>
-              <Label>Spreadsheet:</Label>
-
-              <Input
-                type="file"
-                name="data"
-                id="data"
-                onChange={onFileChange}
-                accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-              />
-              {dataSheet.length !== 0 ? (
+              {dataSheet.length !== 0 && (
                 <div>
-                  <div className="flex flex-row justify-center gap-2 my-2">
-                    <Button
-                      onClick={generateImages}
-                      disabled={isDone || isGenerating}
-                    >
-                      {isGenerating
-                        ? "Generating"
-                        : isDone
-                        ? "Done"
-                        : "Generate Next"}
-                    </Button>
-                    <Button
-                      onClick={async () => {
-                        await zipAndDownloadImages(idImages, "ID Images");
-                      }}
-                    >
-                      Download
-                    </Button>
+                  <div>
+                    {SelectField(
+                      "First Name",
+                      setStudentFirstNameIndex,
+                      dataSheet
+                    )}
+                    {SelectField(
+                      "Middle Name",
+                      setStudentMiddleNameIndex,
+                      dataSheet
+                    )}
+                    {SelectField(
+                      "Last Name",
+                      setStudentLastNameIndex,
+                      dataSheet
+                    )}
+                    {SelectField("Suffix", setStudentSuffixIndex, dataSheet)}
                   </div>
-                  <h1>
-                    {idImages.length.toString()} out of{" "}
-                    {dataSheet.length.toString()} generated
-                  </h1>
+                  {SelectField(
+                    "Guardian Name",
+                    setGuardianNameIndex,
+                    dataSheet
+                  )}
+                  {SelectField(
+                    "Contact Number",
+                    setContactNumberIndex,
+                    dataSheet
+                  )}
+                  {SelectField("Address", setAddressIndex, dataSheet)}
+                  {SelectField("LRN", setLrnIndex, dataSheet)}
                 </div>
-              ) : (
-                <h1 className="my-2 text-center">
-                  Select a Spreadsheet to start
-                </h1>
               )}
               <div>
-                <div className="flex flex-row justify-center items-center my-1">
-                  <Label>STEM?</Label>
-                  <Checkbox
-                    checked={isStem}
-                    onCheckedChange={() => {
-                      setIsStem((state) => !state);
-                    }}
-                  ></Checkbox>
-                </div>
-                <div className="my-1 flex flex-col gap-1">
-                  <Input
-                    type="text"
-                    placeholder="Adviser Name:"
-                    onChange={(e) => setAdviserText(e.target.value)}
-                  ></Input>
-                  <Input
-                    type="text"
-                    placeholder="Section Name:"
-                    onChange={(e) => setSecText(e.target.value)}
-                  ></Input>
-                </div>
-                <div className="flex flex-col gap-2">
-                  <Label>
-                    Photo: x: {photoStyle.x} y: {photoStyle.y} scale:{" "}
-                    {photoStyle.scale}
-                  </Label>
-                  <Slider
-                    defaultValue={[photoStyle.x]}
-                    max={imageRes.height}
-                    onValueChange={(val) => {
-                      setPhotoStyle((state) => ({ ...state, x: val[0] }));
-                    }}
-                  ></Slider>
-                  <Slider
-                    defaultValue={[photoStyle.y]}
-                    max={imageRes.height}
-                    onValueChange={(val) => {
-                      setPhotoStyle((state) => ({ ...state, y: val[0] }));
-                    }}
-                  ></Slider>
-                  <Slider
-                    defaultValue={[photoStyle.scale]}
-                    max={5}
-                    onValueChange={(val) => {
-                      setPhotoStyle((state) => ({ ...state, scale: val[0] }));
-                    }}
-                    step={0.01}
-                  ></Slider>
-                </div>
+                <Label>Student Pictures:</Label>
+                <Input
+                  type="file"
+                  name="images"
+                  onChange={onImagesChange}
+                  multiple
+                  accept="image/*"
+                ></Input>
                 <div>
-                  <Label>
-                    Last Name: x: {lastNameStyle.x} y: {lastNameStyle.y} size:{" "}
-                    {lastNameStyle.fontSize}
-                  </Label>
-                  {Sliders(lastNameStyle, setLastNameStyle)}
+                  <Dialog open={isCropOpen}>
+                    <DialogTrigger asChild>
+                      <div className="flex w-full items-center justify-center">
+                        <Button
+                          className="my-2 "
+                          onClick={() => setIsCropOpen(true)}
+                        >
+                          Edit Image
+                        </Button>
+                      </div>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <Cropper ref={cropperRef} src={photoImage}></Cropper>
+                      <Button onClick={() => onCrop()}>Crop</Button>
+                      <DialogClose asChild>
+                        <Button onClick={() => setIsCropOpen(false)}>
+                          Cancel
+                        </Button>
+                      </DialogClose>
+                    </DialogContent>
+                  </Dialog>
                 </div>
+                <Label>Spreadsheet:</Label>
+                <Input
+                  type="file"
+                  name="data"
+                  id="data"
+                  onChange={onFileChange}
+                  accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                />
+                {dataSheet.length !== 0 ? (
+                  <div>
+                    <div className="flex flex-row justify-center gap-2 my-2">
+                      <Button
+                        onClick={generateImages}
+                        disabled={isDone || isGenerating}
+                      >
+                        {isGenerating
+                          ? "Generating"
+                          : isDone
+                          ? "Done"
+                          : "Generate Next"}
+                      </Button>
+                      <Button
+                        onClick={async () => {
+                          await zipAndDownloadImages(idImages, "ID Images");
+                        }}
+                      >
+                        Download
+                      </Button>
+                    </div>
+                    <h1>
+                      {idImages.length.toString()} out of{" "}
+                      {dataSheet.length.toString()} generated
+                    </h1>
+                  </div>
+                ) : (
+                  <h1 className="my-2 text-center">
+                    Select a Spreadsheet to start
+                  </h1>
+                )}
                 <div>
-                  <Label>
-                    Name: x: {nameStyle.x} y: {nameStyle.y} size:{" "}
-                    {nameStyle.fontSize}
-                  </Label>
-                  {Sliders(nameStyle, setNameStyle)}
-                </div>
-                <div>
-                  <Label>
-                    Section: x: {secStyle.x} y: {secStyle.y}
-                  </Label>
-
-                  {Sliders(secStyle, setSecStyle)}
-                </div>
-                <div>
-                  <Label>
-                    Guardian Name: x: {guardianNameStyle.x} y:{" "}
-                    {guardianNameStyle.y}
-                  </Label>
-
-                  {Sliders(guardianNameStyle, setGuardianNameStyle)}
-                </div>
-                <div>
-                  <Label>
-                    Contact Number: x: {contactNumStyle.x} y:{" "}
-                    {contactNumStyle.y}
-                  </Label>
-
-                  {Sliders(contactNumStyle, setContactNumStyle)}
-                </div>
-                <div>
-                  <Label>
-                    Address: x: {addressStyle.x} y: {addressStyle.y}
-                  </Label>
-
-                  {Sliders(addressStyle, setAddressStyle)}
-                </div>
-                <div>
-                  <Label>
-                    Adviser: x: {adviserStyle.x} y: {adviserStyle.y}
-                  </Label>
-
-                  {Sliders(adviserStyle, setAdviserStyle)}
+                  <div className="flex flex-row justify-center items-center my-1">
+                    <Label>STEM?</Label>
+                    <Checkbox
+                      checked={isStem}
+                      onCheckedChange={() => {
+                        setIsStem((state) => !state);
+                      }}
+                    ></Checkbox>
+                  </div>
+                  <div className="my-1 flex flex-col gap-1">
+                    <Input
+                      type="text"
+                      placeholder="Adviser Name:"
+                      onChange={(e) => setAdviserText(e.target.value)}
+                    ></Input>
+                    <Input
+                      type="text"
+                      placeholder="Section Name:"
+                      onChange={(e) => setSecText(e.target.value)}
+                    ></Input>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <Label>
+                      Photo: x: {photoStyle.x} y: {photoStyle.y} scale:{" "}
+                      {photoStyle.scale}
+                    </Label>
+                    <Slider
+                      defaultValue={[photoStyle.x]}
+                      max={imageRes.height}
+                      onValueChange={(val) => {
+                        setPhotoStyle((state) => ({ ...state, x: val[0] }));
+                      }}
+                    ></Slider>
+                    <Slider
+                      defaultValue={[photoStyle.y]}
+                      max={imageRes.height}
+                      onValueChange={(val) => {
+                        setPhotoStyle((state) => ({ ...state, y: val[0] }));
+                      }}
+                    ></Slider>
+                    <Slider
+                      defaultValue={[photoStyle.scale]}
+                      max={5}
+                      onValueChange={(val) => {
+                        setPhotoStyle((state) => ({ ...state, scale: val[0] }));
+                      }}
+                      step={0.01}
+                    ></Slider>
+                  </div>
+                  <div>
+                    <Label>
+                      Last Name: x: {lastNameStyle.x} y: {lastNameStyle.y} size:{" "}
+                      {lastNameStyle.fontSize}
+                    </Label>
+                    {Sliders(lastNameStyle, setLastNameStyle)}
+                  </div>
+                  <div>
+                    <Label>
+                      Name: x: {nameStyle.x} y: {nameStyle.y} size:{" "}
+                      {nameStyle.fontSize}
+                    </Label>
+                    {Sliders(nameStyle, setNameStyle)}
+                  </div>
+                  <div>
+                    <Label>
+                      Section: x: {secStyle.x} y: {secStyle.y}
+                    </Label>
+                    {Sliders(secStyle, setSecStyle)}
+                  </div>
+                  <div>
+                    <Label>
+                      Guardian Name: x: {guardianNameStyle.x} y:{" "}
+                      {guardianNameStyle.y}
+                    </Label>
+                    {Sliders(guardianNameStyle, setGuardianNameStyle)}
+                  </div>
+                  <div>
+                    <Label>
+                      Contact Number: x: {contactNumStyle.x} y:{" "}
+                      {contactNumStyle.y}
+                    </Label>
+                    {Sliders(contactNumStyle, setContactNumStyle)}
+                  </div>
+                  <div>
+                    <Label>
+                      Address: x: {addressStyle.x} y: {addressStyle.y}
+                    </Label>
+                    {Sliders(addressStyle, setAddressStyle)}
+                  </div>
+                  <div>
+                    <Label>
+                      Adviser: x: {adviserStyle.x} y: {adviserStyle.y}
+                    </Label>
+                    {Sliders(adviserStyle, setAdviserStyle)}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          {/* <div className="grid grid-flow-col ">
+            {idImages.map((val) => {
+              const imageUrl = URL.createObjectURL(val);
+              return <img src={imageUrl} alt="" />;
+            })}
+          </div> */}
         </div>
-        {/* <div className="grid grid-flow-col ">
-          {idImages.map((val) => {
-            const imageUrl = URL.createObjectURL(val);
-            return <img src={imageUrl} alt="" />;
-          })}
-        </div> */}
       </div>
+      <footer className="text-center">Made by ICT Club</footer>
     </div>
   );
 }
