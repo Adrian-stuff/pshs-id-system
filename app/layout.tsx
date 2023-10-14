d03974989bbc34cd6c38fdbac9d8103a7a83d00a
Adrian De Vera <twoadrian6@gmail.com>
2023-10-14 14:08:13 +0800

    remove all ssr changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-"use client";
-
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import NoSsrWrapper from "./no-ssr-wrapper";
-export const dynamic = "force-dynamic";
+
 const inter = Inter({ subsets: ["latin"] });
-const isSSREnabled = () => typeof window === "undefined";
 
 export default function RootLayout({
   children,
@@ -16,9 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>PSHS Layouting System</title>
-      <body className={inter.className}>
-        <NoSsrWrapper>{!isSSREnabled() && children}</NoSsrWrapper>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
