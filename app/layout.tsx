59805fbd91189856340d3b116d985efbb6f4c247
Adrian-stuff <68731858+Adrian-stuff@users.noreply.github.com>
2024-05-13 21:51:50 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
+//please contact adrian de vera for the id layouting system. email: twoadrian6@gmail.com
 export default function RootLayout({
   children,
 }: {
